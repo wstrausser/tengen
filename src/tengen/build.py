082fb78ffefcd3ae7081c201b6da3588e54c736c
William Strausser <william.e.strausser@gmail.com>
2024-10-27 09:20:16 +0000

    Initial commit

---
+++ src/tengen/build.py
@@ -0,0 +1,13 @@
+from commitizen.providers import VersionProvider # type: ignore
+from pathlib import Path
+import os
+
+
+class Provider(VersionProvider):
+    file = Path(os.environ["TENGEN_DIR"], "src", ".version")
+
+    def get_version(self) -> str:
+        return self.file.read_text()
+    
+    def set_version(self, version: str) -> None:
+        self.file.write_text(version)
